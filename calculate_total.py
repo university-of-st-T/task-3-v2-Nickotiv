3e3a58703b0220ed1d053bbd56b9d60789f3a3d4
Nicholas (star) <81709816+Nickotiv@users.noreply.github.com>
2026-03-13 10:44:51 +0300

    Create calculate_total.py

---
+++ calculate_total.py
@@ -0,0 +1,13 @@
+def calculate_total(prices, *discounts, **options):
+    summa = sum(i for i in prices)
+    for i in discounts:
+        summa *= ((100-i)/100)
+    if options:
+        for i in range(len(list(options))):
+            if (list(options)[i]) == "tax":
+                summa *= (1 + (options["tax"])/100)
+            if (list(options)[i]) == "round_to":
+                summa = int(summa) + float(str(summa - int(summa))[:(options["round_to"])+2])
+            else:
+                summa = int(summa) + float(str(summa - int(summa))[:4])
+    return summa
